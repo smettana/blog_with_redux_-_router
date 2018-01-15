0763ea974a32ce4c441a01d04914b47784c35a9c
SASHA <sanya04588@gmail.com>
2018-01-15 18:15:04 +0200

    Adding routing Links Posts component Create post component

---
+++ src/components/posts_index.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
+
+class PostsIndex extends Component {
+
+    componentWillMount(){
+        this.props.fetchPosts();
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="text-xs-right">
+                    <Link to="/posts/new" className="btn btn-primary">
+                        Add a Post
+                    </Link>
+                </div>
+                
+                List of blogposts
+            </div>
+        );
+    }
+    
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        fetchPosts:bindActionCreators(fetchPosts, dispatch)
+    }
+}
+
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
+import { Route, IndexRoute, Router } from 'react-router';
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+import PostsNew from './components/posts_new';
 
-<Route path="/" component={App}/>
+export default (
+    <Route path="/" component={App}>
+        <IndexRoute component={PostsIndex}/>
+        <Route path="posts/new" component={PostsNew}/>
+    </Route>
+)
